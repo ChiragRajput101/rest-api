6c4a867e01ea40307996bf551e157e6138e3a43a
ChiragRajput101 <chiragrajput245@outlook.com>
2024-07-02 01:20:27 +0530

    debugged

--- services/user/store.go
+++ services/user/store.go
@@ -1,11 +1,12 @@
 package user
 
 /* This file isolates the database interaction logic
- from rest of the application logic */
+from rest of the application logic */
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
+	"log"
 
 	"github.com/ChiragRajput101/rest-api/types"
 )
@@ -20,8 +21,49 @@ func NewStore(db *sql.DB) *Store {
 	}
 }
 
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?,?,?,?)", 
+											user.FirstName, user.LastName, user.Email, user.Password)
+	
+	if err != nil {
+		return nil
+	}			
+	return nil
+}
+
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+
+	if err != nil {
+		log.Fatal("query err")
+		return nil, err
+	}
+
+	// fmt.Println("query ok")
+
+	u := new(types.User) // dynamic allocation of the Type, return *Type (0-val)
+
+	for rows.Next() {
+		user, err := scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+		u = user
+	}
+
+	// fmt.Println("ok1")
+
+	if u.ID == 0 || u == nil {
+		return nil, fmt.Errorf("not found")
+	}
+
+	// fmt.Println("ok2")
+
+	return u, nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
 
 	if err != nil {
 		return nil, err
@@ -30,42 +72,33 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	u := new(types.User) // dynamic allocation of the Type, return *Type (0-val)
 
 	for rows.Next() {
-		_, err := scanRowsIntoUserType(rows)
+		_, err := scanRowsIntoUser(rows)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	if u.ID == 0 {
-		return nil, errors.New("user not found")
+		return nil, fmt.Errorf("not found")
 	}
 
 	return u, nil
 }
-
-
-func (s *Store) CreateUser(user types.User) error {
-	return nil
-}
-
-func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
-}
 	
-func scanRowsIntoUserType(rows *sql.Rows) (*types.User, error) {
-	u := new(types.User)
+func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
 
 	err := rows.Scan(
-		&u.ID,
-		&u.FirstName,
-		&u.LastName,
-		&u.Password,
-		&u.CreatedAt,
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
 	)
-
 	if err != nil {
 		return nil, err
 	}
 
-	return u, nil
+	return user, nil
 }
